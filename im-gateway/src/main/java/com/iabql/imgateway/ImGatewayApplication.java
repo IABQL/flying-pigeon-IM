6e0ccf4559b45798ba9e312edec7d6b4f7581572
wuxingcong <wxingcong@163.com>
2022-09-04 17:33:58 +0800

    add gateway

---
+++ im-gateway/src/main/java/com/iabql/imgateway/ImGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.iabql.imgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ImGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImGatewayApplication.class, args);
+    }
+
+}
---
+++ im-gateway/src/test/java/com/iabql/imgateway/ImGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iabql.imgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
